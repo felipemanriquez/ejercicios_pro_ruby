fd532d197e80048955f12a24c628ae10767b5e85
felipemanriquez <zerfunk@gmail.com>
2016-09-26 20:32:58 -0300

    Segundo

--- 2/ej2.rb
+++ 2/ej2.rb
@@ -1,1 +1,34 @@
+require 'pp'
 a = [1,2,3,9,1,4,5,2,3,6,6]
+
+# a.pop
+
+#OTRO
+
+# a.shift
+
+#OTRO
+
+b = a.length
+b= b / 2
+a.delete_at(b)
+#pp a
+
+#OTRO
+
+if a.last != 1 
+	a.pop
+	#puts a
+end
+
+#OTRO
+
+c = []
+a.each do |x|
+	c.unshift(x)
+end
+pp c
+
+
+
+
