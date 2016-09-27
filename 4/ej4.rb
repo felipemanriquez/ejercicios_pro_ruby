895f4eefa543bcde9211f2f94e5fbdbddd4a694a
felipemanriquez <zerfunk@gmail.com>
2016-09-26 21:56:43 -0300

    Cuarto

---
+++ 4/ej4.rb
@@ -0,0 +1,30 @@
+require 'pp'
+
+nombres = ["Violeta", "Andino", "Clemente", "Javiera", "Paula", "Pia",
+"Ray"]
+
+z = []
+# nombres.each_with_index do |x,y|
+
+# 	if x.length <= 5
+# 	z.push(x)
+# 	end
+# end
+# puts z
+
+#OTRO
+
+# z = nombres.select {|b| b.length <=5 }
+# pp z
+
+#OTRO
+# z = nombres.reject {|b| b.length >5 }
+# pp z
+
+#OTRO
+
+z= nombres.map {|a| a.downcase }
+pp z
+
+#OTRO
+
